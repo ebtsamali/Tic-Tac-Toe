13ab32fbbd6d4352f41c25789831a47dbb3ebb9f
M-tarek93 <mti1993@yahoo.com>
2020-01-20 19:37:02 +0200

    finalize server before close

--- gameServer/src/gameserver/ServerManager.java
+++ gameServer/src/gameserver/ServerManager.java
@@ -2,11 +2,13 @@ package gameserver;
 
 import java.time.LocalDateTime;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class ServerManager extends Application {
 
@@ -21,6 +23,11 @@ public class ServerManager extends Application {
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setTitle("Tic Tac Toe Server Manager");
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+       @Override
+       public void handle(WindowEvent e) {
+          System.exit(0);
+       }});
         try{
         stage.getIcons().add(new Image(ServerManager.class.getResourceAsStream("icon.jpg")));
         }catch(Exception e){
